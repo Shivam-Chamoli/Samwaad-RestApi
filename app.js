433395e2c3a39d673e3bd5f0d22d6fe056d8f58c
Shivam-Chamoli <shivamchamoli99@gmail.com>
2021-12-17 10:39:02 +0530

    Fixed bugs in allowed headers

--- app.js
+++ app.js
@@ -51,17 +51,9 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://samwaad-react-app.web.app/"
+    "https://samwaad-react-app.web.app"
   ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
