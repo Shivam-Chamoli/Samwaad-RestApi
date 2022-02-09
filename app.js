6291a6e989575bd46435cf34241d3a8846f287c1
Shivam-Chamoli <shivamchamoli99@gmail.com>
2022-02-09 22:28:20 +0530

    Fixed Allowed Access origin

--- app.js
+++ app.js
@@ -61,14 +61,14 @@ app.use(function (req, res, next) {
   );
   next();
 });
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 //routes
 const usersRoute = require(__dirname + "/routes/users");
 app.use("/api/users", usersRoute);
